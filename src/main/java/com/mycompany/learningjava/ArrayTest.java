8f847602d718bfbf18ef59fd2fa5f2b97fdc5e83
Lenny Gonzalez <badil17@yahoo.com>
2022-10-09 21:09:02 -0400

    Addition of: - Random number generator - min - max search

--- src/main/java/com/mycompany/learningjava/ArrayTest.java
+++ src/main/java/com/mycompany/learningjava/ArrayTest.java
@@ -4,14 +4,40 @@ package com.mycompany.learningjava;/*
  * @author Lenny Gonzalez
  */
 
+import java.util.Random;
+
 public class ArrayTest {
     public static void main(String[] args) {
-        int[] sampleArray = new int[10];
+
+        // Array auto int entry
+        int[] tenNumsArray = new int[10];
         int i;
 
-        for (i = 0; i < 10; i++)
-            sampleArray[i] = i;
-        for (i = 0; i < 10; i++)
-            System.out.println("This is sample[" + i + "]:" + sampleArray[i]);
+        for (i = 0; i < tenNumsArray.length; i++)
+            tenNumsArray[i] = i;
+        for (i = 0; i < tenNumsArray.length; i++)
+            System.out.println("This is sample[" + i + "]:" + tenNumsArray[i]);
+
+        // Array auto random int entry
+        int[] nums = new int[10];
+        int min, max;
+        Random numbersGenerator = new Random();
+
+        for (int j = 0; j < nums.length; j++) {
+            nums[j] = numbersGenerator.nextInt(100);
+            System.out.println(nums[j]);
+        }
+
+        min = max = nums[0]; // Assignment of value from the generated array
+
+        System.out.println(min + " - " + max);
+
+        for (int k = 1; k < 10; k++) { //min and max value search
+            if (nums[k] < min) min = nums[k];
+            if (nums[k] > max) max = nums[k];
+        }
+        System.out.println("minimun number of the array is: " + min + "\nmaximun number of the array is: " + max);
     }
+
+
 }
